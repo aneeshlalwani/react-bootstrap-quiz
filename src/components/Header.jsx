44132487c902b8f9ba165355afa0c2abc68afc29
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2023-11-14 11:15:47 +0500

    created a header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavScrollExample() {
+const Header = () => {
+  const fastechTitle = {
+    color: "#9B51E0",
+  }
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#" style={fastechTitle}>FASTECH</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -17,35 +19,17 @@ function NavScrollExample() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#action1" className='active'>Home</Nav.Link>
+            <Nav.Link href="#action2">My PROFILE</Nav.Link>
+            <Nav.Link href="#action2">ACTIVITY</Nav.Link>
+            <Nav.Link href="#action2">HOW IT WORKS</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+            <Button variant="outline-success">CREARE</Button>
+            <Button variant="outline-success">SIGN IN</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default NavScrollExample;
+export default Header;
